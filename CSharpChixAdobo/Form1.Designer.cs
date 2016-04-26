eebf14fea03b80d9fc43f3ac809c117b15896557
jamesbuchanan007 <jamesbuchanan007@gmail.com>
2016-04-26 11:37:25 -0500

    Added 'Accepts Button' to form

--- CSharpChixAdobo/Form1.Designer.cs
+++ CSharpChixAdobo/Form1.Designer.cs
@@ -190,6 +190,7 @@
             // 
             // mainForm
             // 
+            this.AcceptButton = this.btnCalculate;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
